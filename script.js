91fe614ef547d33cb1e1a5e1ffe75788ce475b64
Surya Sekhar Nath <155349762+GamerLevis27@users.noreply.github.com>
2024-12-26 22:17:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
 }
 
 const getWeatherDetails = (cityName, lat, lon) => {
-    const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
         const uniqueForecastDays = [];
@@ -64,7 +64,7 @@ const getCityCoordinates = () => {
     const cityName = cityInput.value.trim();
     if (!cityName) return;
 
-    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=&appid=${API_KEY}`;
+    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=&appid=${API_KEY}`;
 
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
         if (!data.length) return alert(`No coordinates found for ${cityName}`);
@@ -82,7 +82,7 @@ const getUserCoorinates = () => {
     navigator.geolocation.getCurrentPosition(
         position => {
             const { latitude, longitude} = position.coords;
-            const REVERSE_GEOCODING_URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
+            const REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
             fetch(REVERSE_GEOCODING_URL).then(res => res.json()).then(data => {
                 const { name } = data[0];
                 getWeatherDetails(name, latitude, longitude);
